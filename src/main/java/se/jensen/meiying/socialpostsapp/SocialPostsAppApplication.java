484f4246390adfc9b36b0b08aeb873c411cbff3f
Meiying Hu <meiyinghu18@gmail.com>
2026-01-18 09:01:35 +0100

    first commit

---
+++ src/main/java/se/jensen/meiying/socialpostsapp/SocialPostsAppApplication.java
@@ -0,0 +1,13 @@
+package se.jensen.meiying.socialpostsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPostsAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialPostsAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/jensen/meiying/socialpostsapp/SocialPostsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package se.jensen.meiying.socialpostsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialPostsAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
